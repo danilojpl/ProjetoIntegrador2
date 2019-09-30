717fac0c88b09857a0210a506d983bc753663297
danilojpl <danilo.duartejp@outlook.com>
2019-09-30 16:15:51 -0300

    menu

---
+++ projeto/allegro22/include/objetos.h
@@ -0,0 +1,17 @@
+#ifndef OBJETOS_H_INCLUDED
+#define OBJETOS_H_INCLUDED
+
+
+
+#endif // OBJETOS_H_INCLUDED
+struct NaveEspacial
+{
+ int x;
+ int y;
+ int vidas;
+ int velocidade;
+ int borda_x;
+ int borda_y;
+ int pontos;
+
+};
--- projeto/allegro22/main.c
+++ projeto/allegro22/main.c
@@ -1,10 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 #define LARGURA_TELA 800
 #define ALTURA_TELA 600
 // funções
+struct NaveEspacial
+{
+ int x;
+ int y;
+ int vidas;
+ int velocidade;
+ int borda_x;
+ int borda_y;
+ int pontos;
+
+};
+/*void initNave (NaveEspacial (&nave)){
+    nave.x=20;
+    nave.y=altura_t/2;
+    nave.velocidade=7;
+    nave.borda_x=6;
+    nave.borda_y=7;
+    nave.pontos=0;
+
+} */
 
 int main(void){
 // iniciando
@@ -12,8 +37,20 @@ al_init();
 al_init_image_addon();
 al_install_keyboard();
 al_install_mouse();
+al_init_primitives_addon ();
+al_install_audio();
+al_init_acodec_addon();
+//NaveEspacial nave;
 
 // variaveis
+bool start=false;
+ALLEGRO_AUDIO_STREAM *musica = NULL;
+ALLEGRO_BITMAP *botao=NULL;
+ALLEGRO_BITMAP *botaoA=NULL;
+botao= al_load_bitmap("acesso.png");
+botaoA= al_load_bitmap("apagado.png");
+al_reserve_samples(5);
+ALLEGRO_FONT *fonte = NULL;
 ALLEGRO_BITMAP *imagem = NULL;
 ALLEGRO_DISPLAY *janela = NULL;
 bool fim = false;
@@ -40,22 +77,33 @@ if (!janela)
     al_use_transform(&transformar);
 
 // filas
-fila_eventos = al_create_event_queue();
+    fila_eventos = al_create_event_queue();
 
 //SOURCES
-al_register_event_source(fila_eventos, al_get_keyboard_event_source());
-
+    al_register_event_source(fila_eventos, al_get_keyboard_event_source());
+    al_register_event_source(fila_eventos,al_get_mouse_event_source());
 // codigo
-    imagem = al_load_bitmap("MENU APAGADO.png");
-    al_set_new_display_flags (ALLEGRO_FULLSCREEN_WINDOW);
-    janela = al_create_display(res_x_comp,res_y_comp );
+    musica = al_load_audio_stream("BeepBox-Song.wav", 4, 1024);
+    al_attach_audio_stream_to_mixer(musica, al_get_default_mixer());
+    al_set_audio_stream_playmode(musica, ALLEGRO_PLAYMODE_LOOP);
+    fonte = al_load_font("arial.ttf", 48, 0);
 
-    al_flip_display();
+    imagem = al_load_bitmap("menuteste2.bmp");
+    al_set_new_display_flags (ALLEGRO_FULLSCREEN_WINDOW);
+    janela = al_create_display(res_x_comp,res_y_comp);
+
+    //funções iniciais
+
+   // initNave(nave);
+
     al_clear_to_color(al_map_rgb(255, 255, 255));
-    al_draw_bitmap_region(imagem,0,0,LARGURA_TELA,ALTURA_TELA,0,0,0);
-    al_flip_display();
+   al_draw_bitmap_region(imagem,0,0,LARGURA_TELA,ALTURA_TELA,0,0,0);
+
 
 while(!fim){
+
+
+
     ALLEGRO_EVENT evento_atual;
     al_wait_for_event(fila_eventos, &evento_atual);
     if (evento_atual.type==ALLEGRO_EVENT_KEY_DOWN)
@@ -65,10 +113,48 @@ while(!fim){
         }
 
     }
+    // menu game
+    else if (evento_atual.type == ALLEGRO_EVENT_MOUSE_AXES){
+
+                // Verificamos se ele está sobre a região do retângulo central
+               if (evento_atual.mouse.x >= LARGURA_TELA/2- al_get_bitmap_width(botao) / 2 &&
+                evento_atual.mouse.x <= LARGURA_TELA/2 + al_get_bitmap_width(botao) / 2 &&
+                evento_atual.mouse.y >= ALTURA_TELA/2 - al_get_bitmap_height(botao) / 2 &&
+                evento_atual.mouse.y <= ALTURA_TELA/2 + al_get_bitmap_height(botao) / 2){
+                    al_draw_bitmap(botao,LARGURA_TELA/2-al_get_bitmap_width(botao) / 2,ALTURA_TELA/2 - al_get_bitmap_height(botao) / 2 ,0);
+                }
+                else{
+                    al_draw_bitmap(botaoA,LARGURA_TELA/2-al_get_bitmap_width(botao) / 2,ALTURA_TELA/2 - al_get_bitmap_height(botao) / 2 ,0);
+                }
+
+    }
+    else if (evento_atual.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP)
+        {
+                if (evento_atual.mouse.x >= LARGURA_TELA/2- al_get_bitmap_width(botao) / 2 &&
+                evento_atual.mouse.x <= LARGURA_TELA/2 + al_get_bitmap_width(botao) / 2 &&
+                evento_atual.mouse.y >= ALTURA_TELA/2 - al_get_bitmap_height(botao) / 2 &&
+                evento_atual.mouse.y <= ALTURA_TELA/2 + al_get_bitmap_height(botao) / 2){
+                     al_clear_to_color(al_map_rgb(255, 255, 255));
+                    al_destroy_audio_stream(musica);
+                    al_destroy_bitmap(imagem);
+                    al_destroy_bitmap(botao);
+                    al_destroy_bitmap(botaoA);
+                }
+            }
+
+
+      al_flip_display();
 
 }
+
+
+
 // Finaliza a janela
+al_destroy_audio_stream(musica);
 al_destroy_display(janela);
 al_destroy_event_queue(fila_eventos);
+al_destroy_bitmap(imagem);
+al_destroy_bitmap(botao);
     return 0;
+
 }
